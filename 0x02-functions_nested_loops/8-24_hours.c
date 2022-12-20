6cb10e586d24e8974e1698f5373a035df75bd724
KBrian85 <kiplangat85.brian@gmail.com>
2022-12-20 03:53:37 -0800

    Jack Bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,24 +7,18 @@
  */
 void jack_bauer(void)
 {
-	int s = 0, e = 23, s2 = 0, e2 = 59;
+	int hour, minute;
 
-	while (s <= e)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		while (s2 <= e2)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar(s < 9 ? 0 + '0' : s / 10 + '0');
-			_putchar(s < 9 ? s + '0' : s % 10 + '0');
-			_putchar(":");
-			_putchar(s2 < 9 ? 0 + '0' : s2 / 10 + '0');
-			_putchar(s2 < 9 ? s2 + '0' : s2 % 10 + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar ((minute / 10) + '0');
+			_putchar ((minute % 10) + '0');
 			_putchar('\n');
-
-			s2 * *;
 		}
-		s2 = 0;
-		s++;
 	}
 }
-
-
