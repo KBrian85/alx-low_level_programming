c79f87da1e5790ff73f32319bf2707e0e24574df
KBrian85 <kiplangat85.brian@gmail.com>
2023-04-02 06:01:57 -0700

    0-putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
@@ -15,6 +13,4 @@ int add(int, int);
 void print_to_98(int n);
 void num(int r, int n);
 void print_times_table(int n);
-void main(void);
-#endif
 
