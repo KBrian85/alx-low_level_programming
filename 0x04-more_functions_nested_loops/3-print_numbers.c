9faaa6e9d10e3e51adc61f97bea827576917f4ff
KBrian85 <kiplangat85.brian@gmail.com>
2023-04-03 14:30:48 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the number since 0 upto 0
+ * Return: The number since 0 up to 0
+ */
+
+void print_numbers(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 0; x++)
+	{
+	_putchar(x + "0");
+	}
+	_putchar('\n');
+}
