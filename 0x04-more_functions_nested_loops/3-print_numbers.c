fddae40083aae195ff45593ae955ffe40ff16c25
KBrian85 <kiplangat85.brian@gmail.com>
2023-04-04 03:04:39 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,14 +5,15 @@
  * * Return: The number since 0 up to 9
  */
 
-void print_numbers(void)
+void print_number(void)
 
 {
 	int x;
 
 	for (x = 0; x <= 9; x++)
 	{
-	_putchar(x + "0");
+	_putchar(x + '0');
 	}
 	_putchar('\n');
+
 }
